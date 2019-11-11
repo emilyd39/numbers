dfdbc1ac68c7ed7ab079458aa6be3540ef500997
Emily Dong <edong@chapman.edu>
2019-11-11 13:00:19 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# numbers.sh
+# Emily Dong
+
+echo "Enter a positive number: "
+read NUMBER
+N=1
+while [ $N -le $NUMBER ]
+do
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo $N "Even"
+		N=$[$N+1]
+	else
+		echo $N "Odd"
+		N=$[$N+1]
+	fi
+done
+
